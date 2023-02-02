7deb2407b06c203ab0d6b2bff5bbce5abe190204
Sahil Goel <sahgoel@WKMZT3223957.global.publicisgroupe.net>
2023-02-02 16:00:23 +0000

    initial draft

--- servicelist.js
+++ servicelist.js
@@ -3,32 +3,34 @@ const yaml = require('js-yaml');
 module.exports = {
     getServiceList: function() {
     var serviceList;
+    const jsonDirectory = path.join(process.cwd());
+
     const nodeEnv = process.env.DEPLOY_ENV;    
-    let fileContents = fs.readFileSync('./serviceList-default.yaml', 'utf8');
+    let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-default.yaml', 'utf8');
     let serviceListYaml = yaml.load(fileContents);
     serviceList = serviceListYaml;
     if (nodeEnv === "dev") {
-        let fileContents = fs.readFileSync('./serviceList-dev.yaml', 'utf8');
+        let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-dev.yaml', 'utf8');
         let serviceListYaml = yaml.load(fileContents);
         serviceList = serviceListYaml;
     }
     else if (nodeEnv === "test") {
-        let fileContents = fs.readFileSync('./serviceList-test.yaml', 'utf8');
+        let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-test.yaml', 'utf8');
         let serviceListYaml = yaml.load(fileContents);
         serviceList = serviceListYaml;
     }
     else if (nodeEnv === "pre") {
-        let fileContents = fs.readFileSync('./serviceList-pre.yaml', 'utf8');
+        let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-pre.yaml', 'utf8');
         let serviceListYaml = yaml.load(fileContents);
         serviceList = serviceListYaml;
     }
     else if (nodeEnv === "prod") {
-        let fileContents = fs.readFileSync('./serviceList-prod.yaml', 'utf8');
+        let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-prod.yaml', 'utf8');
         let serviceListYaml = yaml.load(fileContents);
         serviceList = serviceListYaml;
     }
     else if (nodeEnv === "dr") {
-            let fileContents = fs.readFileSync('./serviceList-dr.yaml', 'utf8');
+            let fileContents = fs.readFileSync(jsonDirectory + '/serviceList-dr.yaml', 'utf8');
             let serviceListYaml = yaml.load(fileContents);
             serviceList = serviceListYaml;
         }
